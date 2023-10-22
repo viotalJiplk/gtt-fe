4809c1eefbdd57af5a69b522cedfab96bffd7dd9
viotalJiplk <viotal.varecha@outlook.com>
2023-10-22 10:10:10 +0200

    make embed gdpr compliant

--- src/pages/Home/sections/HowToRegister/HowToRegister.tsx
+++ src/pages/Home/sections/HowToRegister/HowToRegister.tsx
@@ -21,7 +21,7 @@ const HowToRegister: React.FC<RouteComponentProps> = (props) => {
            Pokud Vaše škola není na seznamu pozvaných škol, neváhejte nám napsat na <span className={classes.HowToRegister__link}><a href="mailto:turnajvpocitacovychhrach@gym-tisnov.cz">turnajvpocitacovychhrach@gym-tisnov.cz</a></span>.
            Třeba ji ještě stihneme pozvat!
         </Paragraph>*/}
-        <iframe width={videoWidth} height={3/5*videoWidth} src="https://www.youtube.com/embed/NWt8VuxxHoA?si=ySAP9ieBNp3ASQCc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe width={videoWidth} height={3/5*videoWidth} src="https://www.youtube-nocookie.com/embed/NWt8VuxxHoA?si=3pu4ZfjiZNlPsoww" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         <Paragraph className={classes.HowToRegister__paragraph}>
             Nejdříve si musíte <span className={classes.HowToRegister__link}><NavLink to="/account">vytvořit účet</NavLink></span>. (Nezapomeň vyplnit důležité informace, které o tobě potřebujeme vědět.)
         </Paragraph>
