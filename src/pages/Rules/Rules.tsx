a1e34168edf5f55de5a111c8d3bde672dbe1443d
viotalJiplk <viotal.varecha@outlook.com>
2023-10-27 12:01:44 +0200

    move rules to MarkdownPage

--- src/pages/Rules/Rules.tsx
+++ src/pages/Rules/Rules.tsx
@@ -4,14 +4,39 @@ import Section from '../../components/layout/Section/Section';
 import Heading from '../../components/typography/Heading';
 import { headingTypes } from '../../types/types';
 import { routeTransition, routeVariants } from '../../animations/animations';
-import GeneralRules from './sections/GeneralRules/GeneralRules';
+import Paragraph from "../../components/typography/Paragraph";
+import { GAMENAMES } from "../../constants/constants";
+import { useState, useContext, useEffect } from "react";
+import { Context } from "../../store/context";
+import { useHistory } from "react-router-dom";
+import MarkdownPage from '../../components/MarkdownPage/MarkdownPage';
 
 const Rules = () => {
+    const history = useHistory();
+    const [rules, setRules] = useState<JSX.Element[]>([]);
+    const context = useContext(Context);
+    useEffect(function(){
+        if(context.state.games !== undefined){
+            let games = [];
+            for(let game in context.state.games){
+                let gamename = context.state.games[game].name;
+                let gameDisplay = GAMENAMES[gamename];
+                let url = new URL("/gamepage", window.location.href);
+                url.searchParams.append("gamename", gamename);
+                games.push(<Paragraph key={gamename} className={classes.Rules__paragraph}><a href={url.href}>{gameDisplay}</a></Paragraph>);
+            }
+            setRules(games);
+        }
+    }, [context, history] );
     return <motion.div key="rules" initial="initial" exit="hidden" animate="visible" transition={routeTransition} variants={routeVariants} className={classes.Rules}>
         <Section className={''}>
             <Heading type={headingTypes.main} className={classes.Rules__heading}>Pravidla</Heading>
+            <MarkdownPage pageName="rules" className={classes.Rules__general}></MarkdownPage>
+            <Heading className={classes.Rules__heading} type={headingTypes.h2}>Pravidla pro jednotlivé hry naleznete na jejich stránkách</Heading>
+            <div className={classes.Rules__list}>
+                {rules}
+            </div>
         </Section>
-        <GeneralRules></GeneralRules>
     </motion.div>
 };
 
