6d1408111387342d699816e43c27be67507e77ce
viotalJiplk <viotal.varecha@outlook.com>
2023-11-02 13:36:12 +0100

    add Discord name export

--- src/pages/Contestants/Contestants.tsx
+++ src/pages/Contestants/Contestants.tsx
@@ -10,6 +10,28 @@ import GameSelect from '../../components/form/GameSelect/GameSelect';
 import { Ranks } from '../../constants/constants';
 import { Context } from '../../store/context';
 import CTA from '../../components/layout/CTA/CTA';
+import LoadingSpinner from '../../components/other/Spinner/Spinner';
+import CheckBoxInput from '../../components/form/CheckBoxInput/CheckBoxInput';
+
+interface discordUserObject{
+    id: string;
+    username: string;
+    discriminator: string;
+    global_name: string|"";
+    avatar: string|"";
+    bot?: boolean;
+    system?: boolean;
+    mfa_enabled?: boolean;
+    banner?: string;
+    accent_color?: number;
+    locale?: string;
+    verified?: boolean;
+    email?: string|"";
+    flags?: number;
+    premium_type?: number;
+    public_flags?: number;
+    avatar_decoration_data?: string|"";
+}
 
 interface TeamMember{
     teamId: Number;
@@ -19,11 +41,15 @@ interface TeamMember{
     userId?: Number;
     rank?: Number;
     maxRank?: Number;
+    discordUserObject?: discordUserObject;
 }
 
 const Contestants = () => {
     const context = useContext(Context);
     const [gameId, setGameId] = useState<number|null>(null);
+    const [withDiscord, setWithDiscord] = useState<boolean>(false);
+    const [discord, setDiscord] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
     const [teamsElement, setTeamsElement] = useState<JSX.Element[]>([]);
     const [teams, setTeams] = useState<TeamMember[]>([]);
     const [tableHead, setTableHead] = useState<JSX.Element[]>([]);
@@ -39,7 +65,8 @@ const Contestants = () => {
 
     useEffect(() => {
         if(gameId != null && context.state.games !== undefined){
-            axios.get('/team/list/participating/'+ gameId +'/').then(response => {
+            setLoading(true);
+            axios.get('/team/list/participating/'+ gameId +'/' + withDiscord + '/').then(response => {
                 let tmpTeams: TeamMember[][] = [[]];
                 setTeams(response.data);
                 let tmpTeamElements: JSX.Element[] = [];
@@ -54,6 +81,9 @@ const Contestants = () => {
                     for(let memberId in team){
                         let tableHeadtmp: JSX.Element[] = [<th>Přezdívka</th>, <th>role</th>];
                         let member = team[memberId];
+                        if(member.userId !== undefined){
+                            setDiscord(true);
+                        }
                         tmpTeamElements.push(<tr className={classes.Contestants__member}>
                             <td className={classes.Contestants__member__name}>
                                 {member.nick}
@@ -78,14 +108,25 @@ const Contestants = () => {
                                      member.maxRank !== 0?Ranks[context.state.games[gameId-1].name][member.maxRank]: "Žádný rank"}
                                 </td>
                             }
+                            {member.discordUserObject && member.discordUserObject.global_name && tableHeadtmp.push(<th>Discord Name</th>) &&
+                                <td className={classes.Contestants__member__global_name}>
+                                    {"@" + String(member.discordUserObject.global_name)}
+                                </td>
+                            }
+                            {member.discordUserObject && !member.discordUserObject.global_name &&  member.discordUserObject.username && member.discordUserObject.discriminator !== "0" &&
+                                <td className={classes.Contestants__member__old_name}>
+                                    {member.discordUserObject.username + "#" + member.discordUserObject.discriminator}
+                                </td>
+                            }
                         </tr>);
                         setTableHead(tableHeadtmp);
                     }
                 }
                 setTeamsElement(tmpTeamElements);
-            })
+                setLoading(false);
+            });
         }
-    }, [gameId, context.state.games]);
+    }, [gameId, context.state.games, withDiscord]);
    
     return <motion.div variants={routeVariants} key="contestants" transition={routeTransition} exit="hidden" animate="visible" initial="initial" className={classes.Contestants}>
         <Section className={''}>
@@ -94,7 +135,13 @@ const Contestants = () => {
                 <p className={classes.Contestants__gameLabel}>Hra:</p>
                 <GameSelect setFunction={setGameId} currentGame={gameId} className={classes.Contestants__gameSelectSellector}></GameSelect>
             </div>
-            <table>
+            {discord &&
+                <div className={classes.Contestants__discord}>
+                    <CheckBoxInput setFunction={()=>{setWithDiscord(!withDiscord)}} checked={withDiscord}></CheckBoxInput>
+                    <div>s discord jmény</div>
+                </div>
+            }
+            {!loading && <table>
                 <thead>
                     <tr>
                     {tableHead}
@@ -103,7 +150,8 @@ const Contestants = () => {
                 <tbody>
                 {teamsElement}
                 </tbody>
-            </table>
+            </table>}
+            {loading && <LoadingSpinner></LoadingSpinner>}
             <CTA className={classes.Contestants__cta} onClick={() =>{
                 let csv: String = "";
                 teams.forEach((element)=>{
