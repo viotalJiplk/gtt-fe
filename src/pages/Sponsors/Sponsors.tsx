a19f81e24bc28460a3a083619932ddb0d9808ba8
viotalJiplk <viotal.varecha@outlook.com>
2023-11-08 20:20:54 +0100

    removed static sponsors section

--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -3,7 +3,6 @@ import Heading from '../../components/typography/Heading';
 import { headingTypes } from '../../types/types';
 import { routeTransition, routeVariants } from "../../animations/animations";
 import Section from '../../components/layout/Section/Section';
-import SponsorshipPackages from '../../assets/sponzorske-balicky.png';
 import { motion } from 'framer-motion';
 
 import SponsorsMarkdownPage from '../../components/MarkdownPage/MarkdownPage';
@@ -13,9 +12,6 @@ const Sponsors = () => {
         <Section className={''}>
             <Heading className={''} type={headingTypes.main}>Sponzoři</Heading>
             <SponsorsMarkdownPage pageName='sponsors' className={classes.Sponsors__sponsorsMarkdownPage}></SponsorsMarkdownPage>
-            <Heading className={''} type={headingTypes.h1}>Jak se stát sponzorem?</Heading>
-            <p className={classes.Sponsors__contact}>Pokud máte zájem stát se sponzorem, kontaktujte nás na <b><a href="mailto:turnajvpocitacovychhrach@gym-tisnov.cz">turnajvpocitacovychhrach@gym-tisnov.cz</a></b>.</p>
-            <img className={classes.Sponsors__packages} alt="Sponsorship Packages" src={SponsorshipPackages}></img>
         </Section>
     </motion.div>
 };
